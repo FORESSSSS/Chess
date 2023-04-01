d86943f26b8f3c6907cfaf52f642ac96534cffd2
Fores <draeger.tnt.@yandex.ru>
2023-04-01 14:50:51 +0300

    ChessPiece готов

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,2 +1,22 @@
-public class ChessPiece {
+public abstract class ChessPiece {
+    String color;
+    boolean check = true;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    public abstract String getColor();
+
+    public abstract String getSymbol();
+
+    public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    public boolean checkCage(int toLine, int toColumn) {
+        if (toLine >= 0 && toLine <= 7 && toColumn >= 0 && toColumn <= 7) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
