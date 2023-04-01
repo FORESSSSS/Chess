5e7e4bb48afe0dd7cc520dc743d17e5fb478765f
Fores <draeger.tnt.@yandex.ru>
2023-04-01 15:00:28 +0300

    Bishop готов

---
+++ src/Bishop.java
@@ -0,0 +1,34 @@
+public class Bishop extends ChessPiece {
+    public Bishop(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String getSymbol() {
+
+        return "B";
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        for (int i = 1; i <= 7; i++) {
+            if (toLine == line + i && toColumn == column + i && checkCage(toLine, toColumn)) {
+                return true;
+            } else if (toLine == line + i && toColumn == column - i && checkCage(toLine, toColumn)) {
+                return true;
+            } else if (toLine == line - i && toColumn == column + i && checkCage(toLine, toColumn)) {
+                return true;
+            } else if (toLine == line - i && toColumn == column - i && checkCage(toLine, toColumn)) {
+                return true;
+            } else if (toLine == line && toColumn == column) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
