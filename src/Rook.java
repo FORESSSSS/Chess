6d5a7561e81364990a31951c90a3d8e90654ba1d
Fores <draeger.tnt.@yandex.ru>
2023-04-05 19:00:24 +0300

    доделал Rook

--- src/Rook.java
+++ src/Rook.java
@@ -15,40 +15,32 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-
-        for (int i = 1; i <= 7; i++) {
-            if (toLine == line && toColumn == column + i && checkCage(toLine, toColumn)/*&&move(chessBoard, line, column, toLine,toColumn)*/) {
-                return true;
-            } else if (toLine == line && toColumn == column - i && checkCage(toLine, toColumn)/*&&move(chessBoard, line, column, toLine,toColumn)*/) {
-                return true;
-            } else if (toColumn == column && toLine == line + i && checkCage(toLine, toColumn)/*&&move(chessBoard, line, column, toLine,toColumn)*/) {
-                return true;
-            } else if (toColumn == column && toLine == line - i && checkCage(toLine, toColumn)/*&&move(chessBoard, line, column, toLine,toColumn)*/) {
-                return true;
-            } else if (toColumn == column && toLine == line) {
-                return false;
-            }
+        if (checkCage(toLine, toColumn) && ((line == toLine && column != toColumn) || (line != toLine && column == toColumn))
+                && checkStraightWay(chessBoard, line, column, toLine, toColumn)) {
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
-    public boolean move(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        for(int i =1;i<8;i++){
-            for(int j = 1;j<8;j++){
-
-
-
-        if (chessBoard.board[toLine][toColumn] == null){
-            return true;
-        } else if(chessBoard.board[toLine][toColumn] != null
-    && !chessBoard.board[toLine][toColumn].getColor().equals(chessBoard.board[line][column].getColor())){
-            return true;
-        } else if(chessBoard.board[toLine][toColumn] != null && chessBoard.board[toLine+i][toColumn+j] == null){
-        return false;
-        }else{
-        return false;
+    public boolean checkStraightWay(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if ((line == toLine && column != toColumn)) {
+            for (int i = 1; i < Math.abs(column - toColumn); i++) {
+                if (chessBoard.board[line][Math.min(column, toColumn) + i] != null) {
+                    return false;
+                }
+            }
+        } else if ((line != toLine && column == toColumn)) {
+            for (int i = 1; i < Math.abs(line - toLine); i++) {
+                if (chessBoard.board[Math.min(line, toLine) + i][column] != null) {
+                    return false;
+                }
+            }
         }
-    }}
-    return false;
+        if (chessBoard.board[toLine][toColumn] != null && color.equals(chessBoard.board[toLine][toColumn].getColor())) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
