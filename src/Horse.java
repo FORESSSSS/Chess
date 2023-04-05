3051ea015323339a22ca28a688bc6b8c16388252
Fores <draeger.tnt.@yandex.ru>
2023-04-05 19:03:36 +0300

    доделал Horse

--- src/Horse.java
+++ src/Horse.java
@@ -15,7 +15,7 @@ public class Horse extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (checkCage(toLine, toColumn) && (line != toLine && column != toColumn)
+        if (checkCage(toLine, toColumn) && checkPosition(line, column, toLine, toColumn)
                 && ((Math.abs(line - toLine) == 2 && Math.abs(column - toColumn) == 1) || (Math.abs(line - toLine) == 1 && Math.abs(column - toColumn) == 2))
                 && (chessBoard.board[toLine][toColumn] == null || !color.equals(chessBoard.board[toLine][toColumn].getColor()))) {
             return true;
