09d89f36bea5145f1558cdfc60434fa4d563caf0
Fores <draeger.tnt.@yandex.ru>
2023-04-05 19:03:04 +0300

    доделал Bishop

--- src/Bishop.java
+++ src/Bishop.java
@@ -10,26 +10,37 @@ public class Bishop extends ChessPiece {
 
     @Override
     public String getSymbol() {
-
         return "B";
     }
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (checkCage(toLine, toColumn) && checkPosition(line, column, toLine, toColumn)
+                && (Math.abs(line - toLine) == Math.abs(column - toColumn))
+                && checkDiagonalWay(chessBoard, line, column, toLine, toColumn)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-        for (int i = 1; i <= 7; i++) {
-            if (toLine == line + i && toColumn == column + i && checkCage(toLine, toColumn)) {
-                return true;
-            } else if (toLine == line + i && toColumn == column - i && checkCage(toLine, toColumn)) {
-                return true;
-            } else if (toLine == line - i && toColumn == column + i && checkCage(toLine, toColumn)) {
-                return true;
-            } else if (toLine == line - i && toColumn == column - i && checkCage(toLine, toColumn)) {
-                return true;
-            } else if (toLine == line && toColumn == column) {
-                return false;
+    public boolean checkDiagonalWay(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (line - toLine == column - toColumn) {
+            for (int i = 1; i < Math.abs(line - toLine); i++) {
+                if (chessBoard.board[Math.min(line, toLine) + i][Math.min(column, toColumn) + i] != null) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 1; i < Math.abs(line - toLine); i++) {
+                if (chessBoard.board[Math.min(line, toLine) + i][Math.max(column, toColumn) - i] != null) {
+                    return false;
+                }
             }
         }
-        return false;
+        if (chessBoard.board[toLine][toColumn] != null && color.equals(chessBoard.board[toLine][toColumn].getColor())) {
+            return false;
+        }
+        return true;
     }
 }
